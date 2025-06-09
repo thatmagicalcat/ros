18e8172f56d0cad8d720bf148a3a8a5c3214085e
thatmagicalcat <thatmagicalcat@hotmail.com>
2025-06-09 23:42:49 +0530

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 #![allow(clippy::empty_loop)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test"]
 
 mod panic;
 mod vga_buffer;
@@ -10,7 +13,25 @@ mod macros;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    #[cfg(test)]
+    test();
+
     println!("hello, world!!");
 
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
